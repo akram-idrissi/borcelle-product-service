ec06fd109b8a878133336a638761743fd5bbeff8
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-10 15:28:34 +0100

    Added product controller

---
+++ borcelle/src/main/java/com/borcelle/store/web/ProductController.java
@@ -0,0 +1,12 @@
+package com.borcelle.store.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products/")
+public class ProductController {
+
+
+
+}
