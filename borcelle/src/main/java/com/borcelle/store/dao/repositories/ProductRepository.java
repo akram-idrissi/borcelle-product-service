922ea4a4ccf3a9d7b190eba19e3951770cad0b62
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-10 15:32:51 +0100

    Added new method

--- borcelle/src/main/java/com/borcelle/store/dao/repositories/ProductRepository.java
+++ borcelle/src/main/java/com/borcelle/store/dao/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.borcelle.store.dao.repositories;
 import com.borcelle.store.dao.entities.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends MongoRepository<Product, String> {
+    Optional<Product> findProductByCategory(String category);
 }
