987920206ad6d66237952045c86224b2f945bb48
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-10 14:44:33 +0100

    Added project

---
+++ borcelle/src/main/java/com/borcelle/store/BorcelleApplication.java
@@ -0,0 +1,13 @@
+package com.borcelle.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorcelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorcelleApplication.class, args);
+	}
+
+}
---
+++ borcelle/src/test/java/com/borcelle/store/BorcelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borcelle.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorcelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
