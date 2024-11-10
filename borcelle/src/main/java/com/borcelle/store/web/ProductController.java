b7c362ff19aafb59a821d1de3fb139530c432f44
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-10 16:23:23 +0100

    Added an endpoint

--- borcelle/src/main/java/com/borcelle/store/web/ProductController.java
+++ borcelle/src/main/java/com/borcelle/store/web/ProductController.java
@@ -1,11 +1,25 @@
 package com.borcelle.store.web;
 
+import com.borcelle.store.dao.entities.Product;
+import com.borcelle.store.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/products/")
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public List<Product> fetchProducts() {
+        return productService.getAllProducts();
+    }
 
 
 
