504c2c704999be72c94caa45008fbde15cb33b8b
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-24 14:12:10 +0100

    Using Pageable

--- borcelle/src/main/java/com/borcelle/store/dao/repositories/ProductRepository.java
+++ borcelle/src/main/java/com/borcelle/store/dao/repositories/ProductRepository.java
@@ -1,14 +1,15 @@
 package com.borcelle.store.dao.repositories;
 
 import com.borcelle.store.dao.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
-    List<Product> findAllByCategory(String category);
     Optional<Product> findProductByPid(String pid);
     Optional<Product> findProductBy_id(String pid);
-
+    Page<Product> findAllByCategory(String category, Pageable pageable);
 }
