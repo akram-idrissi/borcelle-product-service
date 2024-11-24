23a779d7c8cb0a70acdb7cb28fa288ed4b8514cd
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-24 14:14:21 +0100

    Using Pageable

--- borcelle/src/main/java/com/borcelle/store/web/ProductController.java
+++ borcelle/src/main/java/com/borcelle/store/web/ProductController.java
@@ -3,12 +3,12 @@ package com.borcelle.store.web;
 import com.borcelle.store.dao.entities.Product;
 import com.borcelle.store.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,8 +19,12 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/")
-    public List<Product> fetchProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<Page<Product>> fetchProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Page<Product> products = productService.getAllProducts(page, size);
+        return ResponseEntity.ok(products);
     }
 
     @GetMapping("/{id}")
@@ -29,8 +33,13 @@ public class ProductController {
     }
 
     @GetMapping("/category/{category}")
-    public List<Product> fetchProductsByCategory(@PathVariable String category) {
-        return productService.getProductsByCategory(category);
+    public ResponseEntity<Page<Product>> fetchProductsByCategory(
+            @PathVariable String category,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Product> products = productService.getProductsByCategory(category, pageable);
+        return ResponseEntity.ok(products);
     }
-
 }
