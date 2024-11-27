b1735b94f2e9ebfbdc106f1db28e50c5f98e8609
Akram Idrissi <akram.mo.gb@gmail.com>
2024-11-27 19:32:49 +0100

    Added open feign annotation

--- borcelle/src/main/java/com/borcelle/store/BorcelleApplication.java
+++ borcelle/src/main/java/com/borcelle/store/BorcelleApplication.java
@@ -2,8 +2,10 @@ package com.borcelle.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BorcelleApplication {
 
 	public static void main(String[] args) {
